aaf6a4d2eb3603dcea6dc6f51c11777488e5628b
oleksandrdiadiun <diadiun.o@ajax.systems>
2023-09-22 18:36:02 +0300

    fix_module_6

--- sort.py
+++ sort.py
@@ -3,52 +3,71 @@ from pathlib import Path
 
 
 def normalize(name):
-    name = ''.join(c if c.isalnum() or c == '.' else '_' for c in name)
-    return name
+    normalized_name = ""
+    for char in name:
+        if char.isalnum():
+            normalized_name += char
+        elif char.isspace() or char == '.':
+            normalized_name += "_"
+        elif char.isalpha():
+            normalized_name += char.encode('ascii', 'ignore').decode()
+        else:
+            normalized_name += "_"
+    return normalized_name
 
 
-def organize_files(folder):
-    for item in folder.iterdir():
+def unpack_archive(archive_path, destination_folder):
+    shutil.unpack_archive(archive_path, destination_folder)
+
+
+def organize_files(source_folder, root_folder):
+
+    categories = {
+        'images': ('.jpeg', '.png', '.jpg', '.svg'),
+        'video': ('.avi', '.mp4', '.mov', '.mkv'),
+        'documents': ('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'),
+        'audio': ('.mp3', '.ogg', '.wav', '.amr'),
+        'archives': ('.zip', '.gz', '.tar')
+    }
+
+    for item in source_folder.iterdir():
         if item.is_dir():
-            organize_files(item)
+            organize_files(item, root_folder)
         else:
             file_extension = item.suffix.lower()
             destination_folder = None
 
-            if file_extension in ('.jpeg', '.png', '.jpg', '.svg'):
-                destination_folder = 'images'
-            elif file_extension in ('.avi', '.mp4', '.mov', '.mkv'):
-                destination_folder = 'video'
-            elif file_extension in ('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'):
-                destination_folder = 'documents'
-            elif file_extension in ('.mp3', '.ogg', '.wav', '.amr'):
-                destination_folder = 'audio'
-            elif file_extension in ('.zip', '.gz', '.tar'):
-                destination_folder = 'archives'
+            for category, extensions in categories.items():
+                if file_extension in extensions:
+                    destination_folder = category
+                    break
 
             if destination_folder:
-                dest_dir = folder / destination_folder
+                dest_dir = root_folder / destination_folder
                 dest_dir.mkdir(parents=True, exist_ok=True)
+
                 new_name = normalize(item.stem) + item.suffix
                 new_path = dest_dir / new_name
-                shutil.move(item, new_path)
-            else:
-                new_name = normalize(item.name)
-                new_path = folder / new_name
-                item.rename(new_path)
+                if destination_folder == 'archives':
+                    unpack_archive(item, new_path)
+                else:
+                    shutil.move(item, new_path)
 
 
-def main(source_folder):
-    source_path = Path(source_folder)
-    if source_path.is_dir():
-        organize_files(source_path)
-        print("Сортування завершено.")
-    else:
-        print("Зазначений шлях не вказує на директорію.")
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python sort.py source_folder")
+        return
+
+    source_folder = Path(sys.argv[1])
+
+    if not source_folder.is_dir():
+        print("The specified path does not point to a directory.")
+        return
+
+    organize_files(source_folder, source_folder)
 
 
 if __name__ == "__main__":
     import sys
-
-    source_folder = sys.argv[1]
-    main(source_folder)
+    main()
